f50381b1a5c98f4387ff0a563460e6f86d946e8a
seineph <79405608+seineph@users.noreply.github.com>
2024-01-27 00:34:23 +0300

    added api key and url

--- sources/script.js
+++ sources/script.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = "8daacfbefa5ad84o0e990686f432t73c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = searchInputElement.value;
+  searchCity(searchInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
