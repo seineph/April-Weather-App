58c9a08397119fbc90681d312d52d0cdf505a3a3
seineph <79405608+seineph@users.noreply.github.com>
2024-01-27 00:44:16 +0300

    added temp. change function

--- sources/script.js
+++ sources/script.js
@@ -1,6 +1,12 @@
+function callWeather(response) {
+  let temperatureDegree = document.querySelector("#temperature");
+  temperatureDegree.innerHTML = Math.round(response.data.temperature.current);
+}
+
 function searchCity(city) {
   let apiKey = "8daacfbefa5ad84o0e990686f432t73c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(callWeather);
 }
 
 function searchSubmit(event) {
