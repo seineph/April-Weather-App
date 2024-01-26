30262ff0da713ce6745e16238544d78781cf35ba
seineph <79405608+seineph@users.noreply.github.com>
2024-01-27 00:26:25 +0300

    added basic js search function

---
+++ sources/script.js
@@ -0,0 +1,9 @@
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
